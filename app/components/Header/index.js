14eff796b18f25c554084c9949c855b284e640c1
Asish Abraham Joseph <aaj013@live.com>
2020-07-30 16:34:04 +0530

    update header

--- app/components/Header/index.js
+++ app/components/Header/index.js
@@ -1,13 +1,14 @@
 import React, { Component } from 'react'
 import { Link } from 'react-router'
 import style from './style.css'
+import logo_mk from '../../assets/logos/mapathon.png'
 
 class Header extends Component {
   render() {
     return (
       <header>
-        <img src="https://tm.icfoss.org/static/media/mapathon.6ff432a8.png" alt="logo" />
-        <h1>Mapathon Keralam</h1>
+        <img src={logo_mk} alt="logo" />
+        <h1>MK Analytics</h1>
         <ul>
           <li><Link to="/" className="link" activeClassName="active">Home</Link></li>
           {/* <li><Link to="/gaps" className="link" activeClassName="active">Gap Detection</Link></li> */}
